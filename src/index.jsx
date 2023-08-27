2aa9cb54c26a72d51cf7f1d12d2df11c813ba2a0
waqas rafiq <wm66179@gmail.com>
2023-08-28 02:26:57 +0500

    Create Posts Like facebook.com news feed

--- src/index.jsx
+++ src/index.jsx
@@ -2,77 +2,69 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import  profileImg  from "./img/profile.jpg";
 
-// import {HandThumbsUp, ChatLeftText, Share } from "react-bootstrap-icons";
+import { GoComment } from 'react-icons/go';
+import { AiOutlineLike } from "react-icons/ai";
+import { PiShareFatDuotone } from "react-icons/pi";
 
-
-function App() {
+function Post(props){
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
+        <div className="post">
+          <div className="postHead">
+            <img src={props.profileImg} width={55} height={55} alt="" />
+            <div>
+              <h4> {props.name} </h4>
+              <div className="date"> {props.date}</div>
+            </div>
+          </div>
+    
+          <p> {props.text}</p>
+    
+          <img className="postImg" src={props.img} alt="" />
+    
+          <div className="postFooter">
+            <div className="button">
+              < AiOutlineLike />
+              Like
+            </div>
+            <div className="button">
+            <GoComment/> Comment
+            </div>
+            <div className="button">
+              < PiShareFatDuotone/>
+             Share
+            </div>
+          </div>
         </div>
-    );
+    );    
 }
-ReactDOM.render(<App />, document.getElementById("root"))
 
-// function post(props){
-//     return (
-//         <div className="post">
-//           <div className="postHead">
-//             <img src={props.profileImg} width={65} height={65} alt="" />
-//             <div>
-//               <h1> {props.name} </h1>
-//               <div className="date"> {props.date}</div>
-//             </div>
-//           </div>
-    
-//           <p> {props.text}</p>
-    
-//           <img className="postImg" src={props.img} alt="" />
-    
-//           <div className="postFooter">
-//             <div className="button">
-//               <HandThumbsUp />
-//               Like
-//             </div>
-//             <div className="button">
-//               <ChatLeftText /> Comment
-//             </div>
-//             <div className="button">
-//               <Share /> Share
-//             </div>
-//           </div>
-//         </div>
-//     );    
-// }
-
-// ReactDOM.render(
-//     <div>
-//       <post
-//         // profileImg={profileImg}
-//         name="Inzamam Malik"
-//         date="3-Jan-2013 4:30 pm"
-//         img="https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-6/353004991_634970768655599_1562410547950250051_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=iP5CdkXBKeQAX9MAERQ&_nc_ht=scontent.fkhi22-1.fna&oh=00_AfCABLXGN6tiaYzYUK_yrCcQnWdrn0KxbFjCjy0ZTlGWSw&oe=64EDD6B0"
-//         text="What’s going on here? Well, at first, it just renders “Hello “ because we’re not passing a name yet. But aside from that…
-//           When React renders a component, it passes the component’s props (short for “properties”) as the first argument, as an object. The props object is just a plain JS object, where the keys are prop names and the values are, well, the values of those props.
-//           You might then wonder, where do props come from? How do we pass them in? Good question."
-//       />
-//       <post
-//         // profileImg={profileImg}
-//         name="Abdul Wahid"
-//         date="20-Jan-2013 5:00 pm"
-//         img="https://i.brecorder.com/primary/2021/06/60d4868b13ca7.jpg"
-//         text="Quick warning though: this tutorial is compleeete. And by that I mean looong. I turned this into a full-fledged free 5-day course, and I made a nice-looking PDF you can read on your iPad or [whatever Android device is cool these days]. Drop your email in the box to get both. (it’s spread over 5 days, but you can jump ahead whenever you want)."
-//       />
-//       <post
-//         // profileImg={profileImg}
-//         name="Shakoor"
-//         date="20-Jan-2013 5:00 pm"
-//         img="https://i.brecorder.com/primary/2021/06/60d4868b13ca7.jpg"
-//         text="Quick warning though: this tutorial is compleeete. And by that I mean looong. I turned this into a full-fledged free 5-day course, and I made a nice-looking PDF you can read on your iPad or [whatever Android device is cool these days]. Drop your email in the box to get both. (it’s spread over 5 days, but you can jump ahead whenever you want)."
-//       />
-//     </div>,
-//     document.querySelector("#root")
-//   );
+ReactDOM.render(
+    <div>
+      <Post
+        profileImg={profileImg}
+        name="Inzamam Malik"
+        date="3-Jan-2013 4:30 pm"
+        img="https://images.unsplash.com/photo-1635530027421-b793c5c8d045?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+        text="What’s going on here? Well, at first, it just renders “Hello “ because we’re not passing a name yet. But aside from that…
+          When React renders a component, it passes the component’s props (short for “properties”) as the first argument, as an object. The props object is just a plain JS object, where the keys are prop names and the values are, well, the values of those props.
+          You might then wonder, where do props come from? How do we pass them in? Good question."
+      />
+      <Post
+        profileImg={profileImg}
+        name="Abdul Wahid"
+        date="20-Jan-2013 5:00 pm"
+        img="https://images.unsplash.com/photo-1583484963886-cfe2bff2945f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+        text="Quick warning though: this tutorial is compleeete. And by that I mean looong. I turned this into a full-fledged free 5-day course, and I made a nice-looking PDF you can read on your iPad or [whatever Android device is cool these days]. Drop your email in the box to get both. (it’s spread over 5 days, but you can jump ahead whenever you want)."
+      />
+      <Post
+        profileImg={profileImg}
+        name="Shakoor"
+        date="20-Jan-2013 5:00 pm"
+        img="https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+        text="Quick warning though: this tutorial is compleeete. And by that I mean looong. I turned this into a full-fledged free 5-day course, and I made a nice-looking PDF you can read on your iPad or [whatever Android device is cool these days]. Drop your email in the box to get both. (it’s spread over 5 days, but you can jump ahead whenever you want)."
+      />
+    </div>,
+    document.querySelector("#root")
+  );
