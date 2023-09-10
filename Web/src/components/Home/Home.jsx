dfdcfc82acee5e9faebe393da2d3fbb967de8fe7
waqas rafiq <wm66179@gmail.com>
2023-09-11 00:09:22 +0500

    Create Dynamic Post And Reusable Component

--- Web/src/components/Home/Home.jsx
+++ Web/src/components/Home/Home.jsx
@@ -1,28 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import InputBox from "../Input/input";
 import PostPopup from "../Popup/postPopup";
-import Posts from "../Posts/posts";
+import {Posts, ZeroPost } from "../Posts/posts";
 import "./Home.css";
-import { useState } from "react";
 
 const Home = () => {
   const [showPopup, setShowPopup] = useState(false);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [posts, setPosts] = useState([]); // Array to store posts
+
   const openPopup = () => {
     setShowPopup(true);
   };
+
   const closePopup = () => {
     setShowPopup(false);
   };
+
+  const addPost = () => {
+    
+    setPostTitle(document.getElementById("username").value);
+    setPostBody(document.getElementById("caption").value);
+    // Check if both postTitle and postBody are not empty before adding the post
+    if (postTitle && postBody) {
+      const newPost = {
+        title: postTitle,
+        body: postBody,
+      };
+      // Add the new post to the posts array
+      setPosts([...posts, newPost]);
+
+      // Clear the input fields
+      setPostTitle("");
+      setPostBody("");
+
+      // Close the popup
+      closePopup();
+    }
+  };
+
   return (
     <div>
-    <div className="mid-col">
+      <div className="mid-col">
         <InputBox onInputClick={openPopup} />
-        <Posts />
-        {showPopup && <PostPopup onClose={closePopup} />}
-    </div>
+        {/* Use .map to render each post */}
+        {posts.length === 0 ? (
+          <ZeroPost />
+        ) : (posts.map((post, index) => (
+          <Posts key={index} postTitle={post.title} postBody={post.body} />
+        )))}
+        {showPopup && (
+          <PostPopup
+            onClose={closePopup}
+            onPost={addPost}
+            postTitle={postTitle}
+            postBody={postBody}
+          />
+        )}
+      </div>
     </div>
   );
 };
-
 
 export default Home;
--- Web/src/components/Posts/posts.jsx
+++ Web/src/components/Posts/posts.jsx
@@ -25,12 +25,10 @@ import {
 import { MdOutlineReportOff, MdOutlineDeleteOutline  } from "react-icons/md";
 import  moment  from "moment";
 
-const Posts = () => {
+const ZeroPost = () => {
   const [show, setShow] = useState(false);
   const handleToggle = () => setShow(!show);
   const timeStamp = moment().fromNow();
-
-
   return (
     <div>
       <Card maxW="lg" m="6">
@@ -68,7 +66,7 @@ const Posts = () => {
             </Menu>
           </Flex>
         </CardHeader>
-        <CardBody paddingTop={-1}>
+        <CardBody paddingTop={0.5}>
           <Collapse startingHeight={50} in={show}>
             Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
             terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
@@ -120,4 +118,92 @@ const Posts = () => {
   );
 };
 
-export default Posts;
+
+const Posts = ({postTitle, postBody}) => {
+  const [show, setShow] = useState(false);
+  const handleToggle = () => setShow(!show);
+  const timeStamp = moment().fromNow();
+  return (
+    <div>
+      <Card maxW="lg" m="6">
+        <CardHeader>
+          <Flex spacing="4">
+            <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+              <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
+
+              <Box>
+                <Heading size="sm">{postTitle}</Heading>
+                <Text fontSize="sm" color="gray.500">
+                  {timeStamp}
+                </Text>
+              </Box>
+            </Flex>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                _hover="null"
+                bg="null"
+                icon={<HamburgerIcon />}
+              />
+              <MenuList>
+                <MenuItem icon={<EditIcon />}>
+                  Edit Post...
+                </MenuItem>
+                <MenuItem icon={<MdOutlineReportOff />}>
+                  Report Post
+                </MenuItem>
+                <MenuItem icon={<MdOutlineDeleteOutline />} >
+                  Delete Post
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </CardHeader>
+        <CardBody paddingTop={0.5}>
+          <Collapse startingHeight={50} in={show}>
+            {postBody}
+          </Collapse>
+          <Button
+            _hover="null"
+            bg="null"
+            size="sm"
+            paddingLeft="-0.5"
+            onClick={handleToggle}
+          >
+            Show {show ? "Less" : "More"}
+          </Button>
+        </CardBody>
+        <Image
+          objectFit="cover"
+          src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          alt="Chakra UI"
+        />
+        <Divider color="blackAlpha" />
+        <CardFooter
+          paddingTop={-0.5}
+          paddingBottom={-0.5}
+          justify="space-between"
+          flexWrap="wrap"
+          sx={{
+            "& > button": {
+              minW: "136px",
+            },
+          }}
+        >
+          <Button flex="1" variant="ghost" leftIcon={<BiLike />}>
+            Like
+          </Button>
+          <Button flex="1" variant="ghost" leftIcon={<BiChat />}>
+            Comment
+          </Button>
+          <Button flex="1" variant="ghost" leftIcon={<BiShare />}>
+            Share
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export { ZeroPost, Posts}  ;
